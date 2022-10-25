818695f1a80df61a59946be3cc126f808cc85236
Narynbek uulu Zhasian <63452238+zhaisan01@users.noreply.github.com>
2022-10-25 17:04:13 +0600

    some changes

--- src/comonents/sidebar/Sidebar.tsx
+++ src/comonents/sidebar/Sidebar.tsx
@@ -2,8 +2,13 @@ import Header from "./components/Header";
 import ListItem from "./components/ListItem";
 import classes from "./Sidebar.module.scss";
 import { mySvg } from "../../assets/img/export";
+import { useDispatch } from "react-redux";
+import { checkAuth } from "../../state/slices/AuthSlice";
+import { AppDispatch } from "../../state/store";
 
 const Sidebar = () => {
+  const dispatch: AppDispatch = useDispatch();
+  dispatch(checkAuth());
   return (
     <div className={classes.sidebar}>
       <Header />
