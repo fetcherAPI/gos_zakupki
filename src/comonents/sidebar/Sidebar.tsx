0f118bdfb1836a335f297d34ff86bcc953a6efcf
Narynbek uulu Zhasian <63452238+zhaisan01@users.noreply.github.com>
2022-10-31 12:28:00 +0600

    sidebar

--- src/comonents/sidebar/Sidebar.tsx
+++ src/comonents/sidebar/Sidebar.tsx
@@ -13,58 +13,12 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { Link } from "react-router-dom";
-import { RouteNames } from "../../routes";
 import { CloseBtnIcon } from "../../assets/img/export";
 import { Icon, icons } from "./icons";
+import { Drawer } from "./Sidebar.styled";
+import { DrawerHeader } from "./Sidebar.styled";
 import Switcher from "../../UIComponents/Input/Switcher";
-
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
+import classes from "./Sidebar.module.scss";
 
 const Sidebar = () => {
   const [open, setOpen] = React.useState(false);
@@ -135,7 +89,7 @@ const Sidebar = () => {
           ))}
         </List>
         {open && (
-          <label>
+          <label className={classes.label_switch}>
             <Switcher from={"Рус"} to={"Кыр"} />
           </label>
         )}
