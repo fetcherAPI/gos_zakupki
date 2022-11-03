dc86f03481c19e7b93e21220a246b9e7265cde2c
Narynbek uulu Zhasian <63452238+zhaisan01@users.noreply.github.com>
2022-11-03 11:42:21 +0600

    styled

--- src/comonents/CreateOrder/Steps/OrganizationInfo.tsx
+++ src/comonents/CreateOrder/Steps/OrganizationInfo.tsx
@@ -4,19 +4,32 @@ import TextField from "@mui/material/TextField";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { Button } from "@material-ui/core";
 import { Field, Form, Formik } from "formik";
 import classes from "./Steps.module.scss";
+import { ImgPickerIcon } from "../../../assets/img/export";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 const OrganizationInfo = () => {
   const [value, setValue] = React.useState<Dayjs | null>(null);
+
   const [orderName, setOrderName] = React.useState<string>("");
+
+  const [age, setAge] = React.useState("");
+
+  const handleChangeSelect = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
 
   const handleChange = (newValue: Dayjs | null) => {
     setValue(newValue);
   };
 
   return (
-    <div>
+    <>
       <Formik
         initialValues={{
           toggle: false,
@@ -33,10 +46,12 @@ const OrganizationInfo = () => {
             <div className={classes.item}>
               <p className={classes.title}>название закупки</p>
               <div className={classes.content}>
-                <textarea
-                  className={classes.texarea}
+                <TextField
+                  fullWidth
                   required
                   onChange={(e) => setOrderName(e.target.value)}
+                  placeholder='текст'
+                  multiline
                 />
               </div>
             </div>
@@ -52,8 +67,9 @@ const OrganizationInfo = () => {
                       inputFormat='MM/DD/YYYY'
                       value={value}
                       onChange={handleChange}
-                      renderInput={(params) => (
+                      renderInput={(params: any) => (
                         <TextField
+                          fullWidth
                           required={true}
                           name='date'
                           {...params}
@@ -74,6 +90,7 @@ const OrganizationInfo = () => {
                       onChange={handleChange}
                       renderInput={(params) => (
                         <TextField
+                          fullWidth
                           required={true}
                           name='date'
                           {...params}
@@ -145,11 +162,119 @@ const OrganizationInfo = () => {
               </div>
             </div>
 
-            <button type='submit'>Submit</button>
+            <div className={classes.item}>
+              <p className={classes.title}>Источники финансирования</p>
+              <div className={classes.content}>
+                <TextField
+                  fullWidth
+                  required
+                  onChange={(e) => setOrderName(e.target.value)}
+                  placeholder='MultiLine with rows: 2 and rowsMax: 4'
+                  multiline
+                />
+              </div>
+            </div>
+
+            <div className={classes.item}>
+              <p className={classes.title}>
+                Основания для выбора <br />
+                ограниченного метода
+              </p>
+              <div className={classes.content}>
+                <TextField
+                  fullWidth
+                  required
+                  onChange={(e) => setOrderName(e.target.value)}
+                  placeholder='MultiLine with rows: 2 and rowsMax: 4'
+                  multiline
+                />
+              </div>
+            </div>
+
+            <div className={classes.item}>
+              <p className={classes.title}>
+                Загрузить обоснование по <br />
+                применению ограниченного <br />
+                метода
+              </p>
+              <div className={classes.content}>
+                <label htmlFor='upload-photo'>
+                  <input
+                    style={{ display: "none" }}
+                    id='upload-photo'
+                    name='upload-photo'
+                    type='file'
+                  />
+                  <Button
+                    fullWidth
+                    color='primary'
+                    variant='contained'
+                    component='span'
+                  >
+                    <ImgPickerIcon />
+                    Прикрепить
+                  </Button>{" "}
+                </label>
+              </div>
+            </div>
+
+            <div className={classes.item}>
+              <p className={classes.title}>
+                Выбор случая применения <br />
+                ограниченного метода
+              </p>
+              <div className={classes.content}>
+                <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+                  <InputLabel id='demo-select-small'>Age</InputLabel>
+                  <Select
+                    fullWidth
+                    labelId='demo-select-small'
+                    id='demo-select-small'
+                    value={age}
+                    label='Age'
+                    onChange={handleChangeSelect}
+                  >
+                    <MenuItem value=''>
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+            </div>
+
+            <div className={classes.item}>
+              <p className={classes.title}>
+                Загрузить обоснование по <br />
+                применению ограниченного <br />
+                метода
+              </p>
+              <div className={classes.content}>
+                <label htmlFor='upload-photo'>
+                  <input
+                    style={{ display: "none" }}
+                    id='upload-photo'
+                    name='upload-photo'
+                    type='file'
+                  />
+                  <Button
+                    fullWidth
+                    color='primary'
+                    variant='contained'
+                    component='span'
+                  >
+                    <ImgPickerIcon />
+                    Прикрепить
+                  </Button>{" "}
+                </label>
+              </div>
+            </div>
           </Form>
         )}
       </Formik>
-    </div>
+    </>
   );
 };
 
