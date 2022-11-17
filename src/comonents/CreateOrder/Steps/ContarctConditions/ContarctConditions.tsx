4e93f8224c03f89300098efddf542a0dd0fff5ae
Narynbek uulu Zhasian <63452238+zhaisan01@users.noreply.github.com>
2022-11-17 15:13:56 +0600

    special conditions contract page finished

--- src/comonents/CreateOrder/Steps/ContarctConditions/ContarctConditions.tsx
+++ src/comonents/CreateOrder/Steps/ContarctConditions/ContarctConditions.tsx
@@ -1,15 +1,6 @@
 import React from "react";
 import { validationRules } from "../AddNewLot/validationShcema";
-import {
-  Form,
-  Button,
-  Checkbox,
-  DatePicker,
-  Input,
-  Select,
-  Space,
-  Radio,
-} from "antd";
+import { Form, Button, Checkbox, DatePicker, Input, Radio } from "antd";
 import { Incoterms } from "../../../Incoterms";
 type Props = {};
 
@@ -28,6 +19,7 @@ export const ContarctConditions = (props: Props) => {
           console.log({ error });
         }}
       >
+        {/* ГАРАНТИЙНОЕ ОБЕСПЕЧЕНИЕ ИСПОЛНЕНИЯ КОНТРАКТА */}
         <Form.Item
           name='ensureContract'
           label='ГАРАНТИЙНОЕ ОБЕСПЕЧЕНИЕ ИСПОЛНЕНИЯ КОНТРАКТА'
@@ -43,6 +35,7 @@ export const ContarctConditions = (props: Props) => {
           />
         </Form.Item>
 
+        {/* УПАКОВКА */}
         <Form.Item
           name='pack'
           label='УПАКОВКА'
@@ -76,15 +69,16 @@ export const ContarctConditions = (props: Props) => {
         </Form.Item>
 
         {/* Если страх.товара есть становится видимым блоком */}
-        <Form.Item
-          name='provideCondition'
-          rules={[
-            validationRules("required", "Это обязательное поле для заполнения"),
-          ]}
-          label='УСЛОВИЕ ПОСТАВКИ'
-        >
-          <Incoterms />
-        </Form.Item>
+        <div>
+          <Form.Item
+            name='provideCondition'
+            rules={[]}
+            label='УСЛОВИЕ ПОСТАВКИ'
+            initialValue={"fds"}
+          >
+            <Incoterms />
+          </Form.Item>
+        </div>
 
         {/* СОПУТСТВУЮЩИЕ УСЛУГИ */}
         <Form.Item
@@ -100,24 +94,30 @@ export const ContarctConditions = (props: Props) => {
           </Radio.Group>
         </Form.Item>
         {/* Если СОПУТСТВУЮЩИЕ УСЛУГИ  есть становится видимым блоком */}
-        <Form.Item
-          name='relatedServicesValue'
-          label='Услуги'
-          rules={[
-            validationRules("required", "Это обязательное поле для заполнения"),
-            validationRules("min", "Минимум 2 сивола", 0, 2),
-            validationRules("max", "Мксимум 1000 сиволов", 1000, 0),
-            validationRules("whitespace", "пустой пробел"),
-          ]}
-        >
-          <TextArea
-            showCount
-            maxLength={1000}
-            placeholder='Укажите способ упаковки'
-            autoSize={{ minRows: 3 }}
-          />
-        </Form.Item>
+        <div>
+          <Form.Item
+            name='relatedServicesValue'
+            label='Услуги'
+            rules={[
+              validationRules(
+                "required",
+                "Это обязательное поле для заполнения"
+              ),
+              validationRules("min", "Минимум 2 сивола", 0, 2),
+              validationRules("max", "Мксимум 1000 сиволов", 1000, 0),
+              validationRules("whitespace", "пустой пробел"),
+            ]}
+          >
+            <TextArea
+              showCount
+              maxLength={1000}
+              placeholder='Укажите способ упаковки'
+              autoSize={{ minRows: 3 }}
+            />
+          </Form.Item>
+        </div>
 
+        {/* СРОК ОПЛАТЫ */}
         <Form.Item
           name='paymentTerm'
           label='СРОК ОПЛАТЫ'
@@ -132,28 +132,236 @@ export const ContarctConditions = (props: Props) => {
           />
         </Form.Item>
 
-        {/* //checked */}
+        {/* НЕУСТОЙКИЗА НЕСВОЕВРЕМЕННУЮ ПОСТАВКУ */}
         <Form.Item
-          name='agreement'
-          wrapperCol={{ span: 24 }}
-          valuePropName='checked'
+          name='delayProvide'
+          label='НЕУСТОЙКИЗА НЕСВОЕВРЕМЕННУЮ ПОСТАВКУ'
           rules={[
-            {
-              validator: (_, value) =>
-                value
-                  ? Promise.resolve()
-                  : Promise.reject(
-                      "To proceed, you need to agree with our terms and conditions"
-                    ),
-            },
+            validationRules("required", "Это обязательное поле для заполнения"),
           ]}
         >
-          <Checkbox>
-            {" "}
-            Agree to our <a href='#'>Terms and Conditions</a>
-          </Checkbox>
+          <Input
+            type='number'
+            placeholder='Введите значение не превышающую 0.1% за каждый день'
+            max={5}
+            min={0}
+            step={0.1}
+          />
         </Form.Item>
 
+        {/* ЗА НЕСВОЕВРЕМЕННУЮ ОПЛАТУ */}
+        <Form.Item
+          name='ensureContract'
+          label='ЗА НЕСВОЕВРЕМЕННУЮ ОПЛАТУ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+          ]}
+        >
+          {/* Максимально вычитаемая сумма */}
+          <p>Максимально вычитаемая сумма</p>
+          <Input
+            type='number'
+            placeholder='Введите значение не превышающую 5% от цены контракта'
+            max={5}
+            min={0}
+            step={0.1}
+          />
+          <br />
+          <br />
+          {/* Ставка за каждый просроченный день */}
+          <p>Ставка за каждый просроченный день</p>
+          <Input
+            type='number'
+            placeholder='Введите значение не превышающую 0.1% за каждый день'
+            max={0.1}
+            min={0}
+            step={0.001}
+          />
+          <br />
+          <br />
+          {/* Максимально вычитаемая сумма */}
+          <p>Максимально вычитаемая сумма</p>
+          <Input
+            type='number'
+            placeholder='Введите значение не превышающую 5% от цены контракта'
+            max={5}
+            min={0}
+            step={0.1}
+          />
+        </Form.Item>
+
+        {/* ТЕХНИЧЕСКИЙ КОНТРОЛЬ И ИСПЫТАНИЯ*/}
+        <Form.Item
+          name='technicalControl'
+          label=' ТЕХНИЧЕСКИЙ КОНТРОЛЬ И ИСПЫТАНИЯ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+          ]}
+        >
+          <Radio.Group>
+            <Radio value={true}>да</Radio>
+            <Radio value={false}>Нет</Radio>
+          </Radio.Group>
+        </Form.Item>
+        {/*Блок для описания ТЕХНИЧЕСКИЙ КОНТРОЛЬ И ИСПЫТАНИЯ*/}
+        <Form.Item
+          name='processTechinalChecking'
+          label='ПРОЦЕДУРЫ ТЕХНИЧЕСКОГО КОНТРОЛЯ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+            validationRules("min", "Минимум 2 сивола", 0, 2),
+            validationRules("max", "Мксимум 1000 сиволов", 1000, 0),
+            validationRules("whitespace", "пустой пробел"),
+          ]}
+        >
+          <TextArea
+            showCount
+            maxLength={1000}
+            placeholder='УКАЖИТЕ ПРОЦЕДУРЫ ТЕХНИЧЕСКОГО КОНТРОЛЯ И ИСПЫТАНИЙ, А ТАКЖЕ ЛЮБЫЕ ИСПЫТАНИЯ ДО ОТГРУЗКИ ТОВАРОВ И ПРИ ОКОНЧАТЕЛЬНОЙ ПРИЕМКЕ'
+            autoSize={{ minRows: 3 }}
+          />
+        </Form.Item>
+        <Form.Item
+          name='placeTechnialCheck'
+          label='ПРОЦЕДУРЫ ТЕХНИЧЕСКОГО КОНТРОЛЯ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+            validationRules("min", "Минимум 2 сивола", 0, 2),
+            validationRules("max", "Мксимум 1000 сиволов", 255, 0),
+            validationRules("whitespace", "пустой пробел"),
+          ]}
+        >
+          <TextArea
+            showCount
+            maxLength={255}
+            placeholder='УКАЖИТЕ МЕСТО ПРЕДПОЛАГАЕМОГО ТЕХ. КОНТРОЛЯ И ИСПЫТАНИЙ'
+            autoSize={{ minRows: 3 }}
+          />
+        </Form.Item>
+
+        {/* ЗАПАСНЫЕ ЧАСТИ*/}
+        <Form.Item
+          name='sparePart'
+          label='ЗАПАСНЫЕ ЧАСТИ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+          ]}
+        >
+          <Radio.Group>
+            <Radio value={true}>да</Radio>
+            <Radio value={false}>Нет</Radio>
+          </Radio.Group>
+        </Form.Item>
+        {/*Блок для описания ЗАПАСНЫЕ ЧАСТИ*/}
+        <Form.Item
+          name='DepsAndExtraRequirmentsForSparePart'
+          label='ПЕРЕЧЕНЬ ЗАПЧАСТЕЙ И ДОПОЛНИТЕЛЬНЫЕ ТРЕБОВАНИЯ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+            validationRules("min", "Минимум 2 сивола", 0, 2),
+            validationRules("max", "Мксимум 1000 сиволов", 255, 0),
+            validationRules("whitespace", "пустой пробел"),
+          ]}
+        >
+          <TextArea
+            showCount
+            maxLength={255}
+            placeholder='ПЕРЕЧЕНЬ ЗАПЧАСТЕЙ И ДОПОЛНИТЕЛЬНЫЕ ТРЕБОВАНИЯ О ЗАПАСНЫХ ЧАСТЯХ, ИЗГОТАВЛИВАЕМЫХ ПОСТАВЩИКОМ'
+            autoSize={{ minRows: 3 }}
+          />
+        </Form.Item>
+
+        {/* ГАРАНТИЙНЫЙ ПЕРИОД*/}
+        <Form.Item
+          name='esure'
+          label='ГАРАНТИЯ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+          ]}
+        >
+          <Radio.Group>
+            <Radio value={true}>да</Radio>
+            <Radio value={false}>Нет</Radio>
+          </Radio.Group>
+        </Form.Item>
+        {/*Блок для описания ГАРАНТИЙНЫЙ ПЕРИОД*/}
+        <Form.Item
+          name='ensurePeriod'
+          label='ГАРАНТИЙНЫЙ ПЕРИОД'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+            validationRules("min", "Минимум 2 сивола", 0, 2),
+            validationRules("max", "Мксимум 1000 сиволов", 500, 0),
+            validationRules("whitespace", "пустой пробел"),
+          ]}
+        >
+          <TextArea
+            showCount
+            maxLength={500}
+            placeholder='Укажите период гарантии выраженный в часах работы или месяцев со дня приёмки товара'
+            autoSize={{ minRows: 3 }}
+          />
+        </Form.Item>
+        <Form.Item
+          name='provideRedressPeriod'
+          label='ОБЕСПЕЧИТЬ ИСПРАВЛЕНИЕ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+            validationRules("min", "Минимум 2 сивола", 0, 2),
+            validationRules("max", "Мксимум 1000 сиволов", 500, 0),
+            validationRules("whitespace", "пустой пробел"),
+          ]}
+        >
+          <Input placeholder='Укажите кол-во дней, за которое поставщик должен исправить дефект или заменить товар' />
+        </Form.Item>
+        <Form.Item
+          name='noMakedEnsureOughts'
+          label='НЕУСТОЙКА ЗА НЕВЫПОЛНЕНИЕ ГАРАНТИЙНЫХ ОБЯЗАТЕЛЬСТВ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+          ]}
+        >
+          <Input
+            type='number'
+            placeholder='Введите в % за каждый день
+            Ставка за каждый просроченный день'
+            max={100}
+            min={0}
+            step={0.1}
+          />
+        </Form.Item>
+        <Form.Item
+          name='maximumDeductSum'
+          label='МАКСИМАЛЬНО ВЫЧИТАЕМАЯ СУММА'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+          ]}
+        >
+          <Input
+            type='number'
+            placeholder='Введите значение не превышающую 10% от цены контракта'
+            max={10}
+            min={0}
+            step={0.1}
+          />
+        </Form.Item>
+        {/* блок выбора документов */}
+
+        {/* УРЕГУЛИРОВАНИЕ СПОРОВ*/}
+        <Form.Item
+          name='disputeControl'
+          label='УРЕГУЛИРОВАНИЕ СПОРОВ'
+          rules={[
+            validationRules("required", "Это обязательное поле для заполнения"),
+          ]}
+        >
+          <Radio.Group>
+            <Radio value={true}>Третейский суд</Radio>
+            <Radio value={false}>
+              Суд общей юрисдикции Кыргызской Республики
+            </Radio>
+          </Radio.Group>
+        </Form.Item>
         <Form.Item wrapperCol={{ span: 24 }}>
           <Button block type='primary' htmlType='submit'>
             Register
